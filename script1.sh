fa9a7959aa4a36226c5578408d5e5962a5cd64e6
Alex Filatov <filicipa@gmail.com>
2022-08-24 19:56:37 +0300

    Igor's 1st lessons

---
+++ script1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ifconfig > ~/IgorExercise/report_$(date +%d_%m_%Y_%H_%M).txt
+
---
+++ script2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#Check the directory
+if [ -d /home/alex/IgorExercise/temp ]; then
+echo > /dev/null
+else
+    mkdir /home/alex/IgorExercise/temp
+fi
+
+read -p "Make a choise ("A"-date or "B"-w)" x
+case $x in
+	A)date > /home/alex/IgorExercise/temp/file-a.txt;;
+	B)w > /home/alex/IgorExercise/temp/file-b.txt
+esac
