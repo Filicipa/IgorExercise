38ce827abf5e953bcbcc94d66819920042759556
Alex Filatov <filicipa@gmail.com>
2022-08-24 22:47:40 +0300

    Next one

--- script2.sh
+++ script2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#Script for manual choose and crate files
+#Script for manual choose and create files
 #Check the directory
-if [ -d /home/alex/IgorExercise/temp ]; then
+if [ -d ./temp ]; then
 echo > /dev/null
 else
     mkdir /home/alex/IgorExercise/temp
 fi
 
-read -p "Make a choise ("A"-date or "B"-w)" x
+read -p "Make a choice ("A"-date or "B"-w)" x
 case $x in
-	A)date > /home/alex/IgorExercise/temp/file-a.txt;;
-	B)w > /home/alex/IgorExercise/temp/file-b.txt
+	A)date > ./temp/file-a.txt;;
+	B)w > ./temp/file-b.txt
 esac
