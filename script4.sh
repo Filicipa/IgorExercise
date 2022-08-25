1aa797fb95e861a8471e584e3c2366a006f69015
Alex Filatov <filicipa@gmail.com>
2022-08-25 17:47:54 +0300

    New script4

--- script4.sh
+++ script4.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-echo "$1"
-echo "$2"
-echo "$3"
-
-
 test $1 || echo "default parameter 1"
 test $2 || echo "default parameter 2"
 test $3 || echo "default parameter 3"
+
+echo "$1"
+echo "$2"
+echo "$3"
