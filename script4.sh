93bcf0f93f550767d28da2e7c5c15d42f77440a7
Alex Filatov <filicipa@gmail.com>
2022-08-25 18:19:39 +0300

    New script4

--- script4.sh
+++ script4.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-test $1 || echo "default-parameter 1"
-test $1 && echo "$1"
-test $2 || echo "default-parameter 2"
-test $2 && echo "$2"
-test $3 || echo "default-parameter 3"
-test $3 && echo "$3"
+a=${1:-"Default-1"}
+b=${2:-'Default-2'}
+c=${3:-"Default-3"}
+
+echo "$a"
+echo "$b"
+echo "$c"
