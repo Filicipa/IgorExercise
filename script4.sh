2b05014b01bf5bd5df3f3072471a1ad49bc7abbc
Alex Filatov <filicipa@gmail.com>
2022-08-25 17:53:11 +0300

    Script4 Good

--- script4.sh
+++ script4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-test $1 || echo "default parameter 1"
-test $2 || echo "default parameter 2"
-test $3 || echo "default parameter 3"
 
-echo "$1"
-echo "$2"
-echo "$3"
+test $1 || echo "default-parameter 1"
+test $1 && echo "$1"
+test $2 || echo "default-parameter 2"
+test $2 && echo "$2"
+test $3 || echo "default-parameter 3"
+test $3 && echo "$3"
