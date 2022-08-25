95a7060442b0c5a34cd75419c6be123a970b561d
Alex Filatov <filicipa@gmail.com>
2022-08-25 16:22:41 +0300

    new

--- script3.sh
+++ script3.sh
@@ -1,27 +1,12 @@
 #!/bin/bash
 #Working with pictures
 
-eden="https://sav1maps.community-server.info/eden2_overhead.jpg"
-izi="https://sav1maps.community-server.info/izi_overhead.jpg"
-kini="https://sav1maps.community-server.info/kinixxx_overhead.jpg"
-
 x=$1
 
-case	$x in
-	eden) echo "$eden";;
-	izi)  echo "$izi";;
-	kini) echo "$kini";;
-	*) echo "Unknow name"
-esac
+y="https://sav1maps.community-server.info/${x}_overhead.jpg"
 
-case $x in
-        eden) wget https://sav1maps.community-server.info/eden2_overhead.jpg;;
-        izi) wget https://sav1maps.community-server.info/izi_overhead.jpg;;
-        kini) wget https://sav1maps.community-server.info/kinixxx_overhead.jpg
-esac
+echo "Download $y"
 
-#name1=eden2_overhead.jpg
-#name2=izi_overhead.jpg
-#name3=kinixxx_overhead.jpg
+wget $y
 
-mv *.jpg ~/images/
+mv *.jpg ../images/
--- script4.sh
+++ script4.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-if	[ "$1" == "one" ]; then
-	echo "One"
-elif	[ "$2" == "two" ]; then
-	echo "Two"
-elif	[ "$3" == "three" ]; then
-	echo "Three"
-else 	echo "Unknow parametr"
-fi
+
